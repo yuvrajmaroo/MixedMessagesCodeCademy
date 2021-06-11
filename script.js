94c9c0771664923345c72b9ffeede2dabd769eed
Yuvraj Maroo <yuvrajmaroo@gmail.com>
2021-06-12 01:15:54 +0530

    Created all the Objects

---
+++ script.js
@@ -0,0 +1,25 @@
+const maincourse= {
+    "White Sauce Pasta":150,
+    "Red Sauce Pasta":150,
+    "Pink Sauce Pasta":150,
+    "Chicken Lasagne":170,
+    "Creamy Mushroom Pasta":120,
+    "Chicken Burger":100,
+    "Cottage Cheese Steak":99,
+}
+const cocktail= {
+    "Beer":70,
+    "Pina Colada":99,
+    "Gin and Tonic":100,
+    "Virgin Mojito":110,
+    "Fruit Punch":50,
+    "White Sangria":110,
+    "Red Sangria":110,
+}
+const Dessert = {
+    "Cheescake":140,
+    "Ice-cream":70,
+    "Rum Ball":80,
+    "Chocolate Ganache":150,
+    "Butterscotch Sundae":120,
+}
